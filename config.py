42aa2b0974091f42e31fd25f957d0e9c3e42570f
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2024-10-12 13:17:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 class Config:
     API_ID = environ.get("API_ID", "28776072")
     API_HASH = environ.get("API_HASH", "b3a786dce1f4e7d56674b7cadfde3c9d")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7902709403:AAHd_YWVe15-b4UaPNmM5JoArgeWix7SOm4") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7933287413:AAHyyX0IdjOk-T3MLD0GvEuEui60ZSKTIfE") 
     BOT_SESSION = environ.get("BOT_SESSION", "forward-bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://ftm:ftm@cluster0.w4syc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
